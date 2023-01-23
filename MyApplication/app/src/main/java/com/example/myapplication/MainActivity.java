0472c73e22e035e530f5e8b6eb6168a516f504cd
RunjieZhao-tech <runjie@seas.upenn.edu>
2023-01-22 20:41:29 -0500

    retrieve the instruction data from web

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,9 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -20,13 +24,22 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.*;
 
 import android.util.*;
 import android.os.*;
@@ -35,10 +48,24 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
 
+import dmax.dialog.SpotsDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private JSONArray output_array = null;
+    List<String> id_list = new ArrayList<>();
+    List<String> instruction_list = new ArrayList<>();
+
+    Map<Integer,String>  id_instruct = new HashMap<>();
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,39 +74,23 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //setSupportActionBar(binding.toolbar);
 
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
+        //run async task and wait till it finishes
+        try {
+            new MyTask().execute().get();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         LinearLayout layout = findViewById(R.id.scroll);
-        //ImageView iv = findViewById(R.id.image1);
-//        Button btnTag = new Button(this);
-//        btnTag.setText("Button 1");
-//        Button btntag1 = new Button(this);
-//        btntag1.setText("Button 2");
-        //ImageView iv = new ImageView(this);
-
-//        ll.addView(btntag1);
-//        ll.addView(btnTag);
-        //Glide.with(this).load("https://spoonacular.com/recipeImages/632925-636x393.jpg").into(iv);
-        //ll.addView(iv);
         SQLiteOpenHelper dbh = DatabaseHelper.getmInstance(this);
         SQLiteDatabase db = dbh.getReadableDatabase();
 
-        //read cvs and input data into database if the data is already inputted, then we will not input the data
-        if(db.isOpen()){
-            Cursor cursor = db.rawQuery("select * from persons",null);
-            if(!cursor.moveToNext()){
+        //read csv and input data into database if the data is already inputted, then we will not input the data
+        if (db.isOpen()) {
+            Cursor cursor = db.rawQuery("select * from persons", null);
+            if (!cursor.moveToNext()) {
                 input_data(db);
             }
         }
@@ -94,24 +105,25 @@ public class MainActivity extends AppCompatActivity {
             while (cursor.moveToNext()) {
                 //search for ingredients
                 int _id = cursor.getInt(0);
-                Cursor ingre_cursor = db.rawQuery("select * from ingredients where recipe_id = "+_id,null);
+                Cursor ingre_cursor = db.rawQuery("select * from ingredients where recipe_id = " + _id, null);
                 String title = cursor.getString(1);
                 String url = cursor.getString(5);
                 String meal_type = cursor.getString(6);
-                Log.d("id-->","id " + _id+ " Name " + title);
-                if(last_id != _id){
+                Log.d("id-->", "id " + _id + " Name " + title);
+                if (last_id != _id) {
                     //Iterate through recipes ingredient database
                     String ingredients = "";
-                    while(ingre_cursor.moveToNext()){
+                    while (ingre_cursor.moveToNext()) {
                         ingredients += ingre_cursor.getString(2) + "(" + ingre_cursor.getDouble(3) + " " + ingre_cursor.getString(4) + "), ";
                     }
-                    ingredients = ingredients.substring(0,ingredients.length()-2) + ".";
+                    ingredients = ingredients.substring(0, ingredients.length() - 2) + ".";
                     ImageView iv = new ImageView(this);
                     TextView tv = new TextView(this);
                     TextView end = new TextView(this);
-                    tv.setText("Name: " + title + "("+ meal_type + ")" + "\nIngredients: " + ingredients);
+                    tv.setText("Name: " + title + "(" + meal_type + ")" + "\nIngredients: " + ingredients);
                     tv.setId(_id);
-                    end.setText("\n");
+
+                    end.setText(id_instruct.get(_id)+"\n");
                     Glide.with(this).load(url).into(iv);
                     layout.addView(tv);
                     layout.addView(iv);
@@ -156,7 +168,8 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    public void input_data(SQLiteDatabase db){
+    //input data into the database
+    public void input_data(SQLiteDatabase db) {
         try {
             InputStreamReader is = null;
             InputStreamReader is_ingre = null;
@@ -166,63 +179,127 @@ public class MainActivity extends AppCompatActivity {
             is_ingre = new InputStreamReader(ingre_input, Charset.forName("UTF-8"));
             BufferedReader reader = new BufferedReader(is);
             BufferedReader reader_ingre = new BufferedReader(is_ingre);
-            reader.readLine();//读取每行
+            reader.readLine();
             String line;
             //input recipe data;
             while ((line = reader.readLine()) != null) {
-                line = line.replace("\"","");
+                line = line.replace("\"", "");
                 String token[] = line.split(",");
                 int i = 0;
                 int id = Integer.parseInt(token[0]);
+                //in order to avoid multiple "," inside the title create i to avoid such situation
                 String title = token[1];
-                if(token.length > 7){
+                if (token.length > 7) {
                     title += "," + token[2];
                     i++;
                 }
-                String subtitle = token[2+i];
-                Log.d("token 3 -->",token[3+i]);
-                int ppl = Integer.parseInt(token[3+i]);
-                double price = Double.parseDouble(token[4+i]);
-                String web = token[5+i];
-                String type = token[6+i];
+                String subtitle = token[2 + i];
+                int ppl = Integer.parseInt(token[3 + i]);
+                double price = Double.parseDouble(token[4 + i]);
+                String web = token[5 + i];
+                String type = token[6 + i];
 
-                //search if the recipes exist
-                Cursor cursor = db.rawQuery("select * from persons where _id = " + id,null);
-                String sql = "insert into persons VALUES(" + id + ",'" +title+ "','" + subtitle + "'," + ppl + "," + price + ",'" + web + "','" + type + "')";
-                if(cursor.moveToNext()){
+                //search if the recipes exist. If exist we will update the query not insert
+                Cursor cursor = db.rawQuery("select * from persons where _id = " + id, null);
+                String sql = "insert into persons VALUES(" + id + ",'" + title + "','" + subtitle + "'," + ppl + "," + price + ",'" + web + "','" + type + "')";
+                if (cursor.moveToNext()) {
                     String last_type = cursor.getString(6);
                     String current_type = type + ", " + last_type;
                     sql = "update persons set meal_type = '" + current_type + "' where _id = " + id;
                 }
                 db.execSQL(sql);
-                Log.d("line -->",line);
-                Log.d("line -->",""+token.length);
             }
 
             //input recipe ingredient data
             int i = 0;
             while ((line = reader_ingre.readLine()) != null) {
-                if(i == 0){
+                if (i == 0) {
                     i++;
                     continue;
                 }
-                line = line.replace("\"","");
+                line = line.replace("\"", "");
                 String token[] = line.split(",");
                 int recipe_id = Integer.parseInt(token[0]);
                 int ingre_id = Integer.parseInt(token[1]);
                 String name = token[2];
-                Log.d("token 3 -->",token[3]);
                 double quantity = Double.parseDouble(token[3]);
                 String measure = token[4];
-                String sql = "insert into ingredients VALUES(" + recipe_id + "," +ingre_id+ ",'" + name + "'," + quantity + ",'" + measure + "')";
+                String sql = "insert into ingredients VALUES(" + recipe_id + "," + ingre_id + ",'" + name + "'," + quantity + ",'" + measure + "')";
                 db.execSQL(sql);
-                Log.d("line -->",line);
-                Log.d("line -->",""+token.length);
             }
             db.close();
         } catch (IOException e) {
             System.out.println("error information");
             e.printStackTrace();
-        };
+        }
     }
+
+    //create subclass of asynctask to retrieve the data
+    class MyTask extends AsyncTask {
+        //准备执行
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        public Object doInBackground(Object[] objects) {
+            //set the path
+            String path="https://admin.mybudgeat.com/codebytes/get-recipe-instructions/";
+            try {
+                URL url = new URL(path);
+                //get the connection
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                //set get or post
+                httpURLConnection.setRequestMethod("GET");
+                //set connection time
+                httpURLConnection.setConnectTimeout(5000);
+                //get the response code
+                int code = httpURLConnection.getResponseCode();
+                if (code==200)
+                {
+                    final InputStream is = httpURLConnection.getInputStream();
+                    StringBuffer stringBuffer = new StringBuffer();
+                    String str=null;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    while ((str=br.readLine())!=null){
+                        stringBuffer.append(str);
+                    }
+
+                    //transfer the string to jsonarray and analysis the array and output the result to arraylist
+                    JSONObject jsobject = JSON.parseObject(stringBuffer.toString());
+                    output_array= jsobject.getJSONArray("data");
+
+                    //Iterate throught jsonarray to retrieve each step
+                    for(int i=0;i<output_array.size();i++)
+                    {
+                        JSONObject object= output_array.getJSONObject(i);
+                        id_list.add(object.getString("recipe_id"));
+                        JSONArray jsa = object.getJSONArray("instructions");
+                        Log.e("Json", "Json: "+object.getString("recipe_id"));
+                        String steps = "";
+                        for(int j=0;j<jsa.size();j++){
+                            JSONObject jso= jsa.getJSONObject(j);
+                            Log.e("Json", "Json: "+jso.getString("instruction"));
+                            steps += "Step" + jso.getString("step") + ": " + jso.getString("instruction") + "\n";
+                        }
+                        id_instruct.put(Integer.parseInt(object.getString("recipe_id")),steps);
+                        instruction_list.add(steps);
+                    }
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            //通知适配器发生改变
+            //取消进度条对话框
+        }
+    }
+
 }
